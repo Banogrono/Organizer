60b97871f5c0c1f3e44d22fa6016e00b9e707827
Banogrono <48155039+Banogrono@users.noreply.github.com>
2022-06-30 14:01:45 +0200

    - refactor CSS - add new icons

--- src/main/java/com/omicron/organizerb/Main.java
+++ src/main/java/com/omicron/organizerb/Main.java
@@ -27,7 +27,7 @@ public class Main extends Application {
         var sceneFxml = new File("src/main/resources/fxml/organizer.fxml").toURI().toURL();
 
         FXMLLoader fxmlLoader = new FXMLLoader(sceneFxml);
-        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 600);
 
         stage.setTitle("Organizer");
         stage.setScene(scene);
